f30d57b7070514f6248d657b5171b507336c7593
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2023-11-21 13:23:36 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,20 @@
-// Using Process stdin
+const readline = require('readline');
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (input) => {
+  if (input.trim() === '') {
+    console.log('Your name is: (no input provided)');
+  } else {
+    console.log(`Your name is: ${input}`);
+  }
+});
+
+rl.on('close', () => {
+  console.log('\nThis important software is now closing');
+});

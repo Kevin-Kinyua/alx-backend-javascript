73269dc6e8bb4954dd33382e24051eb29a3a90dd
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2023-11-21 13:43:23 +0300

    Update on requirement. Listening for exit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,29 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+  });
+
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+
+  // Handling the end of input
+  process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
+
+// Listen for signals to handle program termination
+process.on('SIGINT', () => {
+  process.exit();
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (input) => {
-  if (input.trim() === '') {
-    console.log('Your name is: (no input provided)');
-  } else {
-    console.log(`Your name is: ${input}`);
-  }
-});
-
-rl.on('close', () => {
-  console.log('\nThis important software is now closing');
+process.on('SIGTERM', () => {
+  process.exit();
 });

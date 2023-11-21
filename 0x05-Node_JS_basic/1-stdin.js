ceb69fc13aa646fed806e3f2091cef1429c9f107
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2023-11-21 13:57:28 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,22 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+// 1-stdin.js
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (input) => {
+  if (input.trim() === '') {
+    console.log('Your name is: (no input provided)');
+  } else {
+    console.log(`Your name is: ${input}`);
+  }
+});
+
+rl.on('close', () => {
+  console.log('\nThis important software is now closing');
+});

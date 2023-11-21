c406e780f0bd19f98340bac39b82621561628d13
Kevin-Kinyua <w.kevinkinyua@gmail.com>
2023-11-21 10:04:40 +0000

    Task 1: Using Process stdin Create a program named 1-stdin.js that will be executed through command line:
    It should display the message Welcome to Holberton School, what is your name? (followed by a new line)
    The user should be able to input their name on a new line
    The program should display Your name is: INPUT
    When the user ends the program, it should display This important software is now closing (followed by a new line)
    Requirements:
    
    Your code will be tested through a child process, make sure you have everything you need for that

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,18 @@
 // Using Process stdin
 
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (input) => {
-  if (input.trim() === '') {
-    console.log('Your name is: (no input provided)');
-  } else {
-    console.log(`Your name is: ${input}`);
-  }
-});
-
-rl.on('close', () => {
-  console.log('\nThis important software is now closing');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
